6b4a2d32de1a20e347d860680b3159137a7c3787
SucksToBeAnik <anik.islam1494@gmail.com>
2022-09-18 05:23:20 +0600

    Added heroku provided database

--- keeptrack/settings.py
+++ keeptrack/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -91,12 +91,25 @@ WSGI_APPLICATION = 'keeptrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('NAME'),
+            'USER':os.environ.get('USER'),
+            'PASSWORD':os.environ.get('PASSWORD'),
+            'HOST':os.environ.get('HOST'),
+            'PORT':os.environ.get('PORT')
+        }
+    }
+
 
 
 # Password validation
