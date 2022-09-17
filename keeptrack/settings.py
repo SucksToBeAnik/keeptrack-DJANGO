c2dd1e555c10afc377cfa6220435463e5f591a6c
SucksToBeAnik <anik.islam1494@gmail.com>
2022-09-18 05:28:24 +0600

    bug

--- keeptrack/settings.py
+++ keeptrack/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','keeptrack-io.herokuapp.com']
 
-ALLOWED_HOSTS.append(os.environ.get('ALLOWED_HOST'))
+
 
 
 # Application definition
