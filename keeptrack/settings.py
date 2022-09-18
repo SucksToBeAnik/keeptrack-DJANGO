19fe960571b71f01d91512a60739ded3b5ba9f72
SucksToBeAnik <anik.islam1494@gmail.com>
2022-09-18 07:23:17 +0600

    bug

--- keeptrack/settings.py
+++ keeptrack/settings.py
@@ -92,22 +92,22 @@ WSGI_APPLICATION = 'keeptrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('NAME'),
-            'USER':os.environ.get('USER'),
-            'PASSWORD':os.environ.get('PASSWORD'),
-            'HOST':os.environ.get('HOST'),
-            'PORT':os.environ.get('PORT')
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('NAME'),
+        'USER':os.environ.get('USER'),
+        'PASSWORD':os.environ.get('PASSWORD'),
+        'HOST':os.environ.get('HOST'),
+        'PORT':os.environ.get('PORT')
         }
     }
 
